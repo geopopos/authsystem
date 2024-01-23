e74944226ad9d2ba94fc5300372253499a235960
Toai Quoc Ngo <toaingoquoc00@gmail.com>
2024-01-23 09:57:17 -0400

    Bug fix

--- pages/auth/reset_password.vue
+++ pages/auth/reset_password.vue
@@ -1,46 +1,52 @@
 <script setup lang="ts">
-import type { FormError } from '#ui/types'
+import type { FormError } from "#ui/types";
 
 const client = useSupabaseClient();
 const user = useSupabaseUser();
 const router = useRouter();
+const route = useRoute();
 
-const fields = [{
-  name: 'password',
-  label: 'Password',
-  type: 'password',
-  placeholder: 'Enter your password'
-},
-{
-    name: 'passwordConfirm',
-    label: 'Confirm Password',
-    type: 'password',
-    placeholder: 'Confirm your password'
-}]
+const fields = [
+  {
+    name: "password",
+    label: "Password",
+    type: "password",
+    placeholder: "Enter your password",
+  },
+  {
+    name: "passwordConfirm",
+    label: "Confirm Password",
+    type: "password",
+    placeholder: "Confirm your password",
+  },
+];
 
 const validate = (state: any) => {
-  const errors: FormError[] = []
-  if (!state.password) errors.push({ path: 'password', message: 'Password is required' })
-  if (!state.passwordConfirm) errors.push({ path: 'passwordConfirm', message: 'Password is required' })
-  if (state.password !== state.passwordConfirm) errors.push({ path: 'passwordConfirm', message: 'Passwords do not match' })
+  const errors: FormError[] = [];
+  if (!state.password)
+    errors.push({ path: "password", message: "Password is required" });
+  if (!state.passwordConfirm)
+    errors.push({ path: "passwordConfirm", message: "Password is required" });
+  if (state.password !== state.passwordConfirm)
+    errors.push({ path: "passwordConfirm", message: "Passwords do not match" });
 
-  return errors
-}
+  return errors;
+};
 
-async function onSubmit (data: any) {
+async function onSubmit(data: any) {
   try {
     const { data: updateData, error } = await client.auth.updateUser({
-        password: data.password
-    })
-    console.log('Submitted', data)
-    console.log(updateData)
+      password: data.password,
+    });
+    console.log("Submitted", data);
+    console.log(updateData);
     if (error) {
-      console.log(error)
-      throw error
-    } 
-    router.push('/app')
+      console.log(error);
+      throw error;
+    }
+    router.push("/app");
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 </script>
@@ -58,12 +64,12 @@ async function onSubmit (data: any) {
       :ui="{ base: 'text-center', footer: 'text-center' }"
       @submit="onSubmit"
     >
-
-
       <template #footer>
-        By signing in, you agree to our <NuxtLink to="/" class="text-primary font-medium">Terms of Service</NuxtLink>.
+        By signing in, you agree to our
+        <NuxtLink to="/" class="text-primary font-medium"
+          >Terms of Service</NuxtLink
+        >.
       </template>
     </UAuthForm>
   </UCard>
 </template>
-
